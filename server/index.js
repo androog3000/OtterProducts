c0b5c19e72c66544b29ab0e3ccbec24c60a7c2fe
Andrew Grant <ajgrant318@gmail.com>
2024-12-10 17:59:40 -0800

    modularized route functions

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = 5040;
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
 
-app.listen(5000, () => {
+//fills in URL for routes
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at http://localhost:${PORT}`);
 });
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,74 +1,17 @@
 import express from "express";
 
+import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
+
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send("Welcome to the shire!");
-});
-
-router.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products });
-    } catch (error) {
-        console.log("error fetching products", error.message);
-        res.status(500).json({ success: false, message: "Server error" });
-    }
-}); 
 
 //console.log(process.env.MONGO_URI);
 
-router.get('/products', (req, res) => {
-    res.send("Products coming soon");
-});
+router.get('/', getProducts); 
 
-router.post('/api/products', async (req, res) => {
-    console.log("API POST", req.body, typeof req.body);
-    const product = req.body; // user will send this data
+router.post('/', createProduct);
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields"});
-    }
+router.put('/:id', updateProduct);
 
-    const newProduct = new Product(product);
-
-    try{
-        await newProduct.save();
-        console.log("Product saved: ", product, typeof product);
-        res.status(201).json({ success: true, data: newProduct});
-    } catch (error) {
-        console.error(`Error in creating product ${error.message}`);
-        res.status(500).json({ success: false, message: "Server Error"});
-    }
-});
-
-router.put('/api/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({ success: false, message: "Invalid Product Id"});
-    }
-
-    try{
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-        res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Server error "}); //500 server error
-    }
-})
-
-router.delete("/api/products/:id", async (req, res) => {
-    const {id} = req.params;
-    console.log("id:", id);
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted"});
-    }   catch (error) {
-        console.log("error deleting products", error.message);
-        res.status(404).json({ success: false, message: "Product not found" });
-    }
-});
+router.delete("/:id", deleteProduct);
 
 export default router;
