ada73afee8548905707f6d07f89d8ea2383e0d44
Andrew Grant <ajgrant318@gmail.com>
2024-12-11 22:49:57 -0800

    further updates

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -26,7 +26,7 @@ export const createProduct = async (req, res) => {
 
     try{
         await newProduct.save();
-        //console.log("Product saved: ", product, typeof product);
+        console.log("Product saved:", product);
         res.status(201).json({ success: true, data: newProduct});
     } catch (error) {
         console.error(`Error in creating product ${error.message}`);
@@ -52,11 +52,16 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     const {id} = req.params;
     console.log("id:", id);
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success: false, message: "Invalid Product Id"});
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: "Product deleted"});
     }   catch (error) {
         console.log("error deleting products", error.message);
-        res.status(404).json({ success: false, message: "Product not found" });
+        res.status(500).json({ success: false, message: "Server error" });
     }
 }
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = 5040;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
 
