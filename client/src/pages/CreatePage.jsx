ab7eca3e654b18580557d35da6aef0d2127536eb
Andrew Grant <ajgrant318@gmail.com>
2024-12-11 16:02:30 -0800

    adding front end components from shakraUI and react-icons

---
+++ client/src/pages/CreatePage.jsx
@@ -0,0 +1,24 @@
+import { Container, Heading, VStack } from "@chakra-ui/react";
+
+const CreatePage = () => {
+    const [newProduct, setNewProduct] = useState({
+        name: "",
+        price: "",
+        image: ""
+    });
+
+    return (
+        <Container maxW={"container.sm"}>
+            <VStack
+                spacing={8}
+            >
+            <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                Create New Product
+            </Heading>
+            </VStack>
+
+        </Container>
+    );
+};
+
+export default CreatePage; 
